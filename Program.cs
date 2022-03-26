da77eef39ff88d48475f9d41dcabaa3575cf1c21
Maria Eduarda Casalunga <mariaeduardacasalunga@gmail.com>
2022-03-26 20:42:49 -0300

    finaliza exercício

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿string fraseDigitada, fraseComNumero;
+
+Console.Write("Digite uma frase: ");
+fraseDigitada = Console.ReadLine()!;
+
+fraseComNumero = fraseDigitada
+    .Replace("a","4")
+    .Replace("A","4")
+    .Replace("e","3")
+    .Replace("E","3")
+    .Replace("i","1")
+    .Replace("I","1")
+    .Replace("o","0")
+    .Replace("O","0")
+    .Replace("t","7")
+    .Replace("T","7")
+    .Replace("s","5")
+    .Replace("S","5");
+
+    Console.WriteLine(fraseComNumero);
